13a9d2ed5433ad8ab523081dec2646bac3e9c431
belevtsof <belevtsof@gmail.com>
2020-03-17 11:19:43 +0200

    added service classes

--- src/pojo/coinService.java
+++ src/pojo/coinService.java
@@ -1,5 +1,8 @@
 package pojo;
 
-public class coinService implements Coin {
+public class coinService extends Coin {
 
+    public coinService(long id, String imageUrl, String country, int value, short year, String currency) {
+        super(id, imageUrl, country, value, year, currency);
+    }
 }
---
+++ src/pojo/userService.java
@@ -0,0 +1,8 @@
+package pojo;
+
+public class userService extends User {
+
+    public userService(long id, String imageUrl, String name, String description, String location, int followingCount, int followersCount) {
+        super(id, imageUrl, name, description, location, followingCount, followersCount);
+    }
+}
