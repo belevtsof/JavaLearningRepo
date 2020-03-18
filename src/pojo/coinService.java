76e34062ca2658f57790465c06a7ebf56d7f0eeb
belevtsof <belevtsof@gmail.com>
2020-03-18 15:43:45 +0200

    Removed unnecessery files

--- src/pojo/coinService.java
+++
@@ -1,8 +0,0 @@
-package pojo;
-
-public class coinService extends Coin {
-
-    public coinService(long id, String imageUrl, String country, int value, short year, String currency) {
-        super(id, imageUrl, country, value, year, currency);
-    }
-}
