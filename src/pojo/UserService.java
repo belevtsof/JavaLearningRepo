2f9791e17ff0f479b9f44b443a428215f606c98d
belevtsof <belevtsof@gmail.com>
2020-03-18 15:55:52 +0200

    renamed sevices files

---
+++ src/pojo/UserService.java
@@ -0,0 +1,8 @@
+package pojo;
+
+public class UserService extends User {
+
+    public UserService(long id, String imageUrl, String name, String description, String location, int followingCount, int followersCount) {
+        super(id, imageUrl, name, description, location, followingCount, followersCount);
+    }
+}
--- src/pojo/userService.java
+++
@@ -1,8 +0,0 @@
-package pojo;
-
-public class userService extends User {
-
-    public userService(long id, String imageUrl, String name, String description, String location, int followingCount, int followersCount) {
-        super(id, imageUrl, name, description, location, followingCount, followersCount);
-    }
-}
