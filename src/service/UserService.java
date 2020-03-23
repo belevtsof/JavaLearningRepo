3ef2f20c000aba804e24e2aaa0585398a21b856e
belevtsof <belevtsof@gmail.com>
2020-03-23 13:03:20 +0200

    fixed

---
+++ src/service/UserService.java
@@ -0,0 +1,8 @@
+package service;
+
+public class UserService {
+
+    public UserService(long id, String imageUrl, String name, String description, String location, int followingCount, int followersCount) {
+        super(id, imageUrl, name, description, location, followingCount, followersCount);
+    }
+}
